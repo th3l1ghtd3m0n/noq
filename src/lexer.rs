2e907d5f6a54e618181f9f21a692b4852b6f874d
Lucas Rodrigues <th3l1ghtd3m0n@gmail.com>
2023-05-11 03:47:32 -0300

    feat(src/): update rule definition syntax

--- src/lexer.rs
+++ src/lexer.rs
@@ -25,12 +25,32 @@ impl fmt::Display for Loc
 pub enum TokenKind
 {
     Sym,
+    // Keywords
+    Rule,
+    Shape,
+    Apply,
+    Done,
+    // Special Characters
     OpenParen,
     CloseParen,
     Comma,
     Equals,
     Colon,
+    // Terminators
     Invalid,
+    End,
+}
+
+fn keyword_by_name(text: &str) -> Option<TokenKind>
+{
+    match text
+    {
+        "rule" => Some(TokenKind::Rule),
+        "shape" => Some(TokenKind::Shape),
+        "apply" => Some(TokenKind::Apply),
+        "done" => Some(TokenKind::Done),
+        _ => None,
+    }
 }
 
 impl fmt::Display for TokenKind
@@ -41,12 +61,17 @@ impl fmt::Display for TokenKind
         match self
         {
             Sym => write!(f, "symbol"),
+            Rule => write!(f, "rule keyword"),
+            Shape => write!(f, "shape keyword"),
+            Apply => write!(f, "apply keyword"),
+            Done => write!(f, "done keyword"),
             OpenParen => write!(f, "open paren"),
             CloseParen => write!(f, "close paren"),
             Comma => write!(f, "comma"),
             Equals => write!(f, "equals"),
             Colon => write!(f, "colon"),
             Invalid => write!(f, "invalid token"),
+            End => write!(f, "end of input"),
         }
     }
 }
@@ -62,7 +87,7 @@ pub struct Token
 pub struct Lexer<Chars: Iterator<Item=char>>
 {
     chars: Peekable<Chars>,
-    invalid: bool,
+    exhausted: bool,
     file_path: Option<String>,
     lnum: usize,
     bol: usize,
@@ -75,7 +100,7 @@ impl<Chars: Iterator<Item=char>> Lexer<Chars>
     {
         Self { 
             chars: chars.peekable(), 
-            invalid: false ,
+            exhausted: false ,
             file_path: None,
             lnum: 0,
             bol: 0,
@@ -104,7 +129,7 @@ impl<Chars: Iterator<Item=char>> Iterator for Lexer<Chars>
 
     fn next(&mut self) -> Option<Token>
     {
-        if self.invalid { return None }
+        if self.exhausted { return None }
 
         while let Some(x) = self.chars.next_if(|x| x.is_whitespace())
         {
@@ -117,31 +142,45 @@ impl<Chars: Iterator<Item=char>> Iterator for Lexer<Chars>
         }
 
         let loc = self.loc();
-        let x = self.chars.next()?;
-        self.cnum += 1;
-        let mut text = x.to_string();
-        match x
+        match self.chars.next()
         {
-            '(' => Some(Token {kind: TokenKind::OpenParen, text, loc}),
-            ')' => Some(Token {kind: TokenKind::CloseParen, text, loc}),
-            ',' => Some(Token {kind: TokenKind::Comma, text, loc}),
-            '=' => Some(Token {kind: TokenKind::Equals, text, loc}),
-            ':' => Some(Token {kind: TokenKind::Colon, text, loc}),
-            _ => {
-                if !x.is_alphanumeric()
+            Some(x) => {
+                self.cnum += 1;
+                let mut text = x.to_string();
+                match x
                 {
-                    self.invalid = true;
-                    Some(Token{kind: TokenKind::Invalid, text, loc})
-                } else
-                {
-                    while let Some(x) = self.chars.next_if(|x| x.is_alphanumeric())
-                    {
-                        self.cnum += 1;
-                        text.push(x)
-                    }
+                    '(' => Some(Token {kind: TokenKind::OpenParen, text, loc}),
+                    ')' => Some(Token {kind: TokenKind::CloseParen, text, loc}),
+                    ',' => Some(Token {kind: TokenKind::Comma, text, loc}),
+                    '=' => Some(Token {kind: TokenKind::Equals, text, loc}),
+                    ':' => Some(Token {kind: TokenKind::Colon, text, loc}),
+                    _ => {
+                        if !x.is_alphanumeric()
+                        {
+                            self.exhausted = true;
+                            Some(Token{kind: TokenKind::Invalid, text, loc})
+                        } else
+                        {
+                            while let Some(x) = self.chars.next_if(|x| x.is_alphanumeric())
+                            {
+                                self.cnum += 1;
+                                text.push(x)
+                            }
 
-                    Some(Token{ kind: TokenKind::Sym, text, loc })
+                            if let Some(kind) = keyword_by_name(&text)
+                            {
+                                Some(Token{kind, text, loc})
+                            } else 
+                            {
+                                Some(Token{ kind: TokenKind::Sym, text, loc })
+                            }
+                        }
+                    }
                 }
+            },
+            None => {
+                self.exhausted = true;
+                Some(Token{kind: TokenKind::End, text: "".to_string(), loc})
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,68 @@
-use std::io;
-use std::fmt;
-use std::fs;
 use std::collections::HashMap;
 use std::iter::Peekable;
-use std::io::{stdin, stdout, Write};
+use std::io;
+use std::io::{stdin, stdout};
+use std::io::Write;
+use std::fmt;
+use std::fs;
 
 mod lexer;
+
 use lexer::*;
 
 #[derive(Debug, Clone, PartialEq)]
-enum Expr
-{
+enum Expr {
     Sym(String),
     Fun(String, Vec<Expr>)
 }
 
 #[derive(Debug)]
-enum Error
-{
+enum Error {
     UnexpectedToken(TokenKind, Token),
-    UnexpectedEOF(TokenKind),
     IoError(io::Error),
 }
 
-impl Expr
-{
-    fn parse_peekable(lexer: &mut Peekable<impl Iterator<Item=Token>>) -> Result<Self, Error>
-    {
-        if let Some(name) = lexer.next()
-        {
-            match name.kind
-            {
-                TokenKind::Sym => {
-                    if let Some(_) = lexer.next_if(|t| t.kind == TokenKind::OpenParen)
-                    {
-                        let mut args = Vec::new();
-                        if let Some(_) = lexer.next_if(|t| t.kind == TokenKind::CloseParen)
-                        {
-                            return Ok(Expr::Fun(name.text, args))
-                        }
-                        args.push(Self::parse_peekable(lexer)?);
-                        while let Some(_) = lexer.next_if(|t| t.kind == TokenKind::Comma)
-                        {
-                            args.push(Self::parse_peekable(lexer)?);
-                        }
-                        if let Some(t) = lexer.peek()
-                        {
-                            if t.kind == TokenKind::CloseParen
-                            {
-                                lexer.next();
-                                Ok(Expr::Fun(name.text, args))
-                            } else
-                            {
-                                Err(Error::UnexpectedToken(TokenKind::CloseParen, t.clone()))
-                            }
-                        } else
-                        {
-                            Err(Error::UnexpectedEOF(TokenKind::CloseParen))
-                        }
-                    } else
-                    {
-                        Ok(Expr::Sym(name.text))
+impl Expr {
+    fn parse_peekable(lexer: &mut Peekable<impl Iterator<Item=Token>>) -> Result<Self, Error> {
+        let name = lexer.next().expect("Completely exhausted lexer");
+
+        match name.kind {
+            TokenKind::Sym => {
+                if let Some(_) = lexer.next_if(|t| t.kind == TokenKind::OpenParen) {
+                    let mut args = Vec::new();
+                    if let Some(_) = lexer.next_if(|t| t.kind == TokenKind::CloseParen) {
+                        return Ok(Expr::Fun(name.text, args))
                     }
-                },
-                _ => Err(Error::UnexpectedToken(TokenKind::Sym, name))
-            }
-        } else
-        {
-            Err(Error::UnexpectedEOF(TokenKind::Sym))
+                    args.push(Self::parse_peekable(lexer)?);
+                    while let Some(_) = lexer.next_if(|t| t.kind == TokenKind::Comma) {
+                        args.push(Self::parse_peekable(lexer)?);
+                    }
+                    let close_paren = lexer.next().expect("Completely exhausted lexer");
+                    if close_paren.kind == TokenKind::CloseParen {
+                        Ok(Expr::Fun(name.text, args))
+                    } else {
+                        Err(Error::UnexpectedToken(TokenKind::CloseParen, close_paren))
+                    }
+                } else {
+                    Ok(Expr::Sym(name.text))
+                }
+            },
+            _ => Err(Error::UnexpectedToken(TokenKind::Sym, name))
         }
     }
 
-    fn parse(lexer: &mut impl Iterator<Item=Token>) -> Result<Self, Error>
-    {
+    fn parse(lexer: &mut impl Iterator<Item=Token>) -> Result<Self, Error> {
         Self::parse_peekable(&mut lexer.peekable())
     }
 }
 
-impl fmt::Display for Expr
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
-    {
-        match self
-        {
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
             Expr::Sym(name) => write!(f, "{}", name),
             Expr::Fun(name, args) => {
                 write!(f, "{}(", name)?;
-                for (i, arg) in args.iter().enumerate()
-                {
+                for (i, arg) in args.iter().enumerate() {
                     if i > 0 { write!(f, ", ")? }
                     write!(f, "{}", arg)?;
                 }
@@ -98,36 +73,30 @@ impl fmt::Display for Expr
 }
 
 #[derive(Debug)]
-struct Rule
-{
+struct Rule {
     head: Expr,
-    body: Expr
+    body: Expr,
 }
 
-fn substitute_bindings(bindings: &Bindings, expr: &Expr) -> Expr
-{
+fn substitute_bindings(bindings: &Bindings, expr: &Expr) -> Expr {
     use Expr::*;
-    match expr
-    {
+    match expr {
         Sym(name) => {
-            if let Some(value) = bindings.get(name)
-            {
+            if let Some(value) = bindings.get(name) {
                 value.clone()
-            } else
-            {
+            } else {
                 expr.clone()
             }
         },
+
         Fun(name, args) => {
-            let new_name = match bindings.get(name)
-            {
+            let new_name = match bindings.get(name) {
                 Some(Sym(new_name)) => new_name.clone(),
                 None => name.clone(),
                 Some(_) => panic!("Expected symbol in the place of the functor name"),
             };
             let mut new_args = Vec::new();
-            for arg in args
-            {
+            for arg in args {
                 new_args.push(substitute_bindings(bindings, &arg))
             }
             Fun(new_name, new_args)
@@ -135,43 +104,33 @@ fn substitute_bindings(bindings: &Bindings, expr: &Expr) -> Expr
     }
 }
 
-fn expect_token_kind(lexer: &mut Peekable<impl Iterator<Item=Token>>, kind: TokenKind) -> Result<Token, Error>
-{
-    let token = lexer.next().ok_or(Error::UnexpectedEOF(kind))?;
-    if token.kind == kind
-    {
+fn expect_token_kind(lexer: &mut Peekable<impl Iterator<Item=Token>>, kind: TokenKind) -> Result<Token, Error> {
+    let token = lexer.next().expect("Completely exhausted lexer");
+    if token.kind == kind {
         Ok(token)
-    } else
-    {
+    } else {
         Err(Error::UnexpectedToken(kind, token))
     }
 }
 
-impl Rule
-{
-    fn parse(lexer: &mut Peekable<impl Iterator<Item=Token>>) -> Result<Rule, Error>
-    {
+impl Rule {
+    fn parse(lexer: &mut Peekable<impl Iterator<Item=Token>>) -> Result<Rule, Error> {
         let head = Expr::parse_peekable(lexer)?;
         expect_token_kind(lexer, TokenKind::Equals)?;
         let body = Expr::parse_peekable(lexer)?;
         Ok(Rule{head, body})
     }
 
-    fn apply_all(&self, expr: &Expr) -> Expr
-    {
-        if let Some(bindings) = pattern_match(&self.head, expr)
-        {
+    fn apply_all(&self, expr: &Expr) -> Expr {
+        if let Some(bindings) = pattern_match(&self.head, expr) {
             substitute_bindings(&bindings, &self.body)
-        } else
-        {
+        } else {
             use Expr::*;
-            match expr
-            {
+            match expr {
                 Sym(_) => expr.clone(),
                 Fun(name, args) => {
                     let mut new_args = Vec::new();
-                    for arg in args
-                    {
+                    for arg in args {
                         new_args.push(self.apply_all(arg))
                     }
                     Fun(name.clone(), new_args)
@@ -181,46 +140,35 @@ impl Rule
     }
 }
 
-impl fmt::Display for Rule
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
-    {
+impl fmt::Display for Rule {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} = {}", self.head, self.body)
     }
 }
 
 type Bindings = HashMap<String, Expr>;
 
-fn pattern_match(pattern: &Expr, value: &Expr) -> Option<Bindings>
-{
-    fn pattern_match_impl(pattern: &Expr, value: &Expr, bindings: &mut Bindings) -> bool
-    {
+fn pattern_match(pattern: &Expr, value: &Expr) -> Option<Bindings> {
+    fn pattern_match_impl(pattern: &Expr, value: &Expr, bindings: &mut Bindings) -> bool {
         use Expr::*;
-        match (pattern, value)
-        {
+        match (pattern, value) {
             (Sym(name), _) => {
-                if let Some(bound_value) = bindings.get(name)
-                {
+                if let Some(bound_value) = bindings.get(name) {
                     bound_value == value
-                } else
-                {
+                } else {
                     bindings.insert(name.clone(), value.clone());
                     true
                 }
             },
             (Fun(name1, args1), Fun(name2, args2)) => {
-                if name1 == name2 && args1.len() == args2.len()
-                {
-                    for i in 0..args1.len()
-                    {
-                        if !pattern_match_impl(&args1[i], &args2[i], bindings)
-                        {
+                if name1 == name2 && args1.len() == args2.len() {
+                    for i in 0..args1.len() {
+                        if !pattern_match_impl(&args1[i], &args2[i], bindings) {
                             return false;
                         }
                     }
                     true
-                } else
-                {
+                } else {
                     false
                 }
             },
@@ -230,22 +178,17 @@ fn pattern_match(pattern: &Expr, value: &Expr) -> Option<Bindings>
 
     let mut bindings = HashMap::new();
 
-    if pattern_match_impl(pattern, value, &mut bindings)
-    {
+    if pattern_match_impl(pattern, value, &mut bindings) {
         Some(bindings)
-    } else
-    {
+    } else {
         None
     }
 }
 
 #[allow(unused_macros)]
-macro_rules! fun_args
-{
+macro_rules! fun_args {
     () => { vec![] };
-    ($name:ident) => {
-        vec![expr!($name)]
-    };
+    ($name:ident) => { vec![expr!($name)] };
     ($name:ident,$($rest:tt)*) => {
         {
             let mut t = vec![expr!($name)];
@@ -266,8 +209,7 @@ macro_rules! fun_args
 }
 
 #[allow(unused_macros)]
-macro_rules! expr
-{
+macro_rules! expr {
     ($name:ident) => {
         Expr::Sym(stringify!($name).to_string())
     };
@@ -277,22 +219,22 @@ macro_rules! expr
 }
 
 #[cfg(test)]
-mod tests
-{
+mod tests {
     use super::*;
 
     #[test]
-    pub fn rule_apply_all()
-    {
+    pub fn rule_apply_all() {
+        // swap(pair(a, b)) = pair(b, a)
         let swap = Rule {
             head: expr!(swap(pair(a, b))),
             body: expr!(pair(b, a)),
         };
 
-        let input = expr!{
+        let input = expr! {
             foo(swap(pair(f(a), g(b))),
                 swap(pair(q(c), z(d))))
         };
+
         let expected = expr! {
             foo(pair(g(b), f(a)),
                 pair(z(d), q(c)))
@@ -302,8 +244,7 @@ mod tests
     }
 }
 
-fn parse_rules_from_file(file_path: &str) -> Result<HashMap<String, Rule>, Error>
-{
+fn parse_rules_from_file(file_path: &str) -> Result<HashMap<String, Rule>, Error> {
     let mut rules = HashMap::new();
     let source = fs::read_to_string(file_path).map_err(|e| Error::IoError(e))?;
     let mut lexer = {
@@ -312,10 +253,13 @@ fn parse_rules_from_file(file_path: &str) -> Result<HashMap<String, Rule>, Error
         lexer.peekable()
     };
 
-    while let Some(_) = lexer.peek()
-    {
+    while let Some(token) = lexer.peek() {
+        if token.kind == TokenKind::End {
+            break;
+        }
+
+        expect_token_kind(&mut lexer, TokenKind::Rule)?;
         let name = expect_token_kind(&mut lexer, TokenKind::Sym)?;
-        expect_token_kind(&mut lexer, TokenKind::Colon)?;
         let rule = Rule::parse(&mut lexer)?;
         rules.insert(name.text, rule);
     }
@@ -323,11 +267,9 @@ fn parse_rules_from_file(file_path: &str) -> Result<HashMap<String, Rule>, Error
     Ok(rules)
 }
 
-fn main()
-{
+fn main() {
     let default_rules_path = "src/rules.noq";
-    let rules = match parse_rules_from_file(default_rules_path)
-    {
+    let rules = match parse_rules_from_file(default_rules_path) {
         Ok(rules) => {
             println!("INFO: successfully loaded rules from {}", default_rules_path);
             rules
@@ -341,16 +283,11 @@ fn main()
                       actual.loc, expected, actual.kind, actual.text);
             Default::default()
         }
-        Err(Error::UnexpectedEOF(expected)) => {
-            eprintln!("ERROR: expected {} but got nothing", expected);
-            Default::default()
-        }
     };
 
     println!("Available rules:");
-    for (name, rule) in rules
-    {
-        println!("{} : {}", name, rule);
+    for (name, rule) in rules {
+        println!("rule {} {}", name, rule);
     }
 
     let swap = Rule {
@@ -358,6 +295,7 @@ fn main()
         body: expr!(pair(b, a)),
     };
     let mut command = String::new();
+
     let prompt = "> ";
 
     loop {
@@ -365,19 +303,14 @@ fn main()
         print!("{}", prompt);
         stdout().flush().unwrap();
         stdin().read_line(&mut command).unwrap();
-        match Expr::parse(&mut Lexer::from_iter(command.chars()))
-        {
+        match Expr::parse(&mut Lexer::from_iter(command.chars())) {
             Ok(expr) => println!("{}", swap.apply_all(&expr)),
             Err(Error::UnexpectedToken(expected, actual)) => {
                 println!("{:>width$}^", "", width=prompt.len() + actual.loc.col);
                 println!("ERROR: expected {} but got {} '{}'", expected, actual.kind, actual.text)
-            },
-            Err(Error::UnexpectedEOF(expected)) => {
-                println!("{:>width$}^", "", width=prompt.len() + command.len());
-                println!("ERROR: expected {} but got nothing", expected)
             }
-            Err(Error::IoError(io_error)) => {
-                unreachable!("IO ERROR: {}", io_error)
+            Err(err) => {
+                unreachable!("ERROR: {:?}", err)
             }
         }
     }
